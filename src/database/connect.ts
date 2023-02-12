fd4cee0a95b062d6f3ea4bf13a7e7af2649da28f
ahmed eid <ahmedeid6842@gmail.com>
2023-02-12 09:28:50 +0200

    intial typeorm setup & connect to postgresql

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,12 +1,13 @@
-import { Pool } from "pg";
+import { DataSource } from "typeorm";
 import config from "config";
 
-const pool = new Pool({
-  user: config.get<string>("PG_USER"),
+export default new DataSource({
+  type: "postgres",
+  username: config.get<string>("PG_USER"),
   host: config.get<string>("PG_HOST"),
   database: config.get<string>("PG_DATABASE"),
   port: config.get<number>("PG_PORT"),
   password: config.get<string>("PG_PASSWORD"),
+  synchronize: process.env.NODE_ENV !== "production",
+  logging: process.env.NODE_ENV !== "production",
 });
-
-export default pool;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import "express-async-errors";
 import express from "express";
 import config from "config";
-import User from "./routes/user";
-import Movie from "./routes/movie";
-import Customer from "./routes/customer";
-import Genre from "./routes/genre";
-import Rental from "./routes/rental";
+import createDataBaseConnection from "./database/connect";
+// import User from "./routes/user";
+// import Movie from "./routes/movie";
+// import Customer from "./routes/customer";
+// import Genre from "./routes/genre";
+// import Rental from "./routes/rental";
 import { log } from "./utils/logger";
-import { errorHandlerMiddleware } from "./middleware/errorHandler";
+// import { errorHandlerMiddleware } from "./middleware/errorHandler";
 const app = express();
 
 process.on("uncaughtException", (ex: any) => {
@@ -20,13 +21,16 @@ process.on("unhandledRejection", (ex: any) => {
   process.exit(1);
 });
 
-app.use(express.json());
-app.use("/api/user", User);
-app.use("/api/movie", Movie);
-app.use("/api/customer", Customer);
-app.use("/api/genre", Genre);
-app.use("/api/rental", Rental);
-app.use(errorHandlerMiddleware);
+// app.use(express.json());
+// app.use("/api/user", User);
+// app.use("/api/movie", Movie);
+// app.use("/api/customer", Customer);
+// app.use("/api/genre", Genre);
+// app.use("/api/rental", Rental);
+// app.use(errorHandlerMiddleware);
 
 const port = config.get<number>("PORT") || 3000;
-app.listen(port, () => console.log(`server start at port ${port} 🚀 🎯`));
+createDataBaseConnection.initialize().then((conn) => {
+  log.info(`connected postgres DataBase : ${conn.options.database}  🐘`);
+  app.listen(port, () => log.info(`server start at port ${port} 🚀 🎯`));
+});
