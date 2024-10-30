c58deaf498444faed1c4b67ea04a1272b226788f
Ahmed Eid <ahmedeid6842@gmail.com>
2024-10-31 00:41:55 +0300

    chore: Add unit tests for token utility functions

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,14 +2,15 @@ import jwt from "jsonwebtoken";
 import config from "config";
 import { jwtPayload } from "../types/jwtPayload";
 
-const JWT_SECRET = config.get<string>("JWT_SECRET");
 
 export const createToken = (payload: jwtPayload) => {
+  const JWT_SECRET = config.get<string>("JWT_SECRET");
   return jwt.sign(payload, JWT_SECRET, { expiresIn: "15m" });
 };
 
 export const verifyToken = (token: string) => {
   try {
+    const JWT_SECRET = config.get<string>("JWT_SECRET");
     return jwt.verify(token, JWT_SECRET);
   } catch (error) {
     throw new Error("invalid Token Provided");
